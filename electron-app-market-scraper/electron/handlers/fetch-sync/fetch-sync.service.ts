b96e0373255b7cc5e754896d3de26aaa4ac401f8
Piootrekk <piotr.wilkowiecki100@gmail.com>
2026-02-03 23:57:02 +0100

    added sync hander

--- electron-app-market-scraper/electron/handlers/fetch-sync/fetch-sync.service.ts
+++ electron-app-market-scraper/electron/handlers/fetch-sync/fetch-sync.service.ts
@@ -1,1 +1,18 @@
-const fetchSyncService = () => {};
+import { getDbInstance } from "../../db.config";
+import { getCountIdsFromAccount } from "./fetch-sync.repository";
+import { firstListingsFetch } from "../../core/domain/fetch-market-listings/fetch-queue";
+import type { ProgressEmitter } from "./fetch-sync.emits";
+const fetchSyncService = async (
+  progressEmitter: ProgressEmitter,
+  steamid: string,
+  cookies: string,
+) => {
+  const db = getDbInstance();
+  const listingsAmount = await getCountIdsFromAccount(db, steamid);
+  const { totalCount, listings } = await firstListingsFetch(
+    cookies,
+    (message, status) => _,
+  );
+};
+
+export { fetchSyncService };
